f7fccd2e92ab5931fb7f100271a9f4bfb674e824
DouglasRivera <73321495+DouglasRivera@users.noreply.github.com>
2021-07-23 15:49:53 -0500

    Lab terminado

--- main.cpp
+++ main.cpp
@@ -1,21 +1,44 @@
 #include <iostream>
-
+#include <math.h>
 using namespace std;
 
 int menu();
+void Sumatoria(int);
+void Operacion(int);
 
+void Sumatoria(int n){
+	double resultado=0;
+	for	(int i = 1; i<= n; i++){
+		double num = 2.0 * i - 1.0;
+		double den = i * (i + 1.0);
+		resultado += num/den;
+	}
+	cout << "S("<< n <<")" << resultado <<endl;
+}
 
+void Operacion(int x){
+	double resultado = 1.0 / (1.0 + exp(x));
+	cout << "f("<< x <<")" << resultado <<endl;
+}
 int main() {
 
 
 int opcion = menu();
 switch (opcion){
 	case 1:{
-		
+		int n = 0;
+		while (n < 1){
+			cout << "Ingrese un numero: ";
+			cin >> n;
+		}
+		Sumatoria(n);
 		break;
 }
 	case 2:{
-		  
+		  int x = 0;
+			cout << "Ingrese un numero: ";
+			cin >> x;
+		    Operacion(x);
 		break;
 	}
 	case 3:{
